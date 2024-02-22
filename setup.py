2d1b2d8f38fec8bd4842a85f6a621ba00cdab9a0
Canoming <canoming@163.com>
2024-02-22 21:50:17 +0800

    update dependence

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup,find_packages
 
 setup(name='QCircLearning',
-      version='0.2',
+      version='0.3',
       description='Qiskit based variational circuit',
-      url='https://github.com/Canoming/CircTraining',
+      url='https://github.com/Canoming/QCircLearning',
       author='Canoming',
       author_email='canoming@163.com',
       license='MIT',
       packages=find_packages(exclude=["*.tests","*.tests.*","tests.*","tests"]),
       install_requires=[
-          'qiskit >= 0.4.0',
+          'qiskit >= 0.45.0, < 1.0.0',
+          'keras >= 3.0.0',
+          'pylatexenc >= 2.8',
+          'matplotlib >= 3.0.0',
       ],
       zip_safe=False)
