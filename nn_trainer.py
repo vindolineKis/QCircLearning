faec4274a0f98d0b9ba455eac59b50cf68dfc0f8
GUO <544829646@qq.com>
2024-11-15 17:38:23 +0800

    pythorch version

--- nn_trainer.py
+++ nn_trainer.py
@@ -1,139 +1,47 @@
-import tensorflow as tf
-
-import keras
-# from keras.src.models.sequential import Sequential
-from keras.models import Sequential
-
-from keras import layers as klayers
-from keras import optimizers as koptimizers
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 from scipy.optimize import minimize
-# from tensorflow.keras.optimizers import Adam
-from scipy.optimize import approx_fprime
-from scipy.misc import derivative
-# from tensorflow.keras.optimizers import Adam
-from scipy.optimize import approx_fprime
-from scipy.misc import derivative
-
 from typing import List, Callable
-from keras import backend as K
 
-from keras import backend as K
+class TrainerModel(nn.Module):
+    def __init__(self, layers: List[nn.Module] = None, name: str = None):
+        super(TrainerModel, self).__init__()
+        self.model = nn.Sequential(*layers)
+        self.name = name
+        self.double()
 
+    def forward(self, x):
+        return self.model(x)
 
-class trainer_model(Sequential):
-
-    '''
-    A class that inherits from keras.models.Sequential and adds a back_minimize method to minimize the output of the model by training the input.
-
-    Parameters:
-    layers: List[keras.layers.Layer]
-        A list of keras layers to be added to the model.
-    trainable: bool
-        Whether the model is trainable or not.
-    name: str
-        The name of the model.
-
-    Returns:  trainer_model
-    
-    '''
-
-    K.set_floatx('float64') 
-    def __init__(self,
-                 layers:klayers.Layer=None,
-                 trainable:bool=True,
-                 name:str=None,
-                 ):
-        super().__init__(layers=layers, name=name)
-   
-    def back_minimize(self,
-                 x0:np.ndarray=None,
-                 method = 'L-BFGS-B', verbose = 0):
-        """
-        After the model is trained, minimize the output by training the input.
-        """
+    def back_minimize(self, x0: np.ndarray = None, method='L-BFGS-B', verbose=0):
         x_list = []
         f_list = []
         gradient_list = []
-        x_list = []
-        f_list = []
-        gradient_list = []
-        # # @tf.function
+
         def to_minimize(x):
-            # pad_x = np.array([x])
-            return self(x)
-            # pad_x = np.array([x])
-            return self(x)
+            x_tensor = torch.tensor(x, dtype=torch.float64, requires_grad=True)
+            return self(x_tensor).detach().numpy()
 
         if x0 is None:
-            x = np.random.rand(self.inputs[0].shape[1])
+            x = np.random.rand(self.model[0].in_features)
         else:
             x = x0
+
         def to_minimize_with_grad(x):
             x0 = x
-            x = tf.Variable([x0])
+            x_tensor = torch.tensor(x0, dtype=torch.float64, requires_grad=True)
             x_list.append(x0)
-            with tf.GradientTape() as tape:
-                tape.watch(x)
-                loss = to_minimize(x)
-                f_list.append(loss.numpy()[0][0])
-            gradients = tape.gradient(loss, x)
+            loss = self(x_tensor)
+            f_list.append(loss.item())
+            loss.backward()
+            gradients = x_tensor.grad.numpy()
             gradient_list.append(gradients)
-            # print("shape of the gradients:", gradients.numpy().shape())
+            return loss.item(), gradients
 
-            if gradients is None:
-                raise ValueError("Gradient calculation failed; TensorFlow returned None.")
-
-            gradients = gradients.numpy()[0]
-
-
-            return loss.numpy()[0][0], gradients
-        
-        def diff_grad(x, f, h):
-            delta = np.zeros_like(x)
-            delta[0] = h
-
-            grad = []
-
-            for i in range(len(x)):
-                d = np.roll(delta, i)
-                grad.append((f(x + d) - f(x - d)) / (2 * h))
-
-            return np.array(grad)
-        
-        def finite_difference_grad(x, h=1e-6):
-            # print("self(x)is:",self(x))
-            f = lambda x: to_minimize(np.atleast_2d(x)).numpy()[0][0]
-            # f = to_minimize(x).numpy()[0][0]
-            # f = lambda x: to_minimize(x)
-            # f = self([x])
-            gradients = approx_fprime(x, f, h)
-            gradients_m = diff_grad(x, f, h)
-          
-            # gradients_m = gradients.numpy
-            loss = f(x)
-            return loss, gradients,gradients_m
-        
-        def compare_gradients(x):
-            _, tf_grad = to_minimize_with_grad(x)
-            _, fd_grad ,fd_grad_m= finite_difference_grad(x, h=1e-6)
-            
-            print("TensorFlow gradient:", tf_grad)
-            # print(tf_grad.shape())
-            print("Finite difference gradient:", fd_grad)
-            print("Finite difference gradient_m:", fd_grad_m)
-            # print(fd_grad.shape())
-            
-            grad_diff = np.linalg.norm(tf_grad - fd_grad)
-            print(f"Gradient difference (L2 norm): {grad_diff}")
-            return grad_diff
-        
-        print('the difference between the gradients of the finite difference and the tensorflow is:', compare_gradients(x))
-
-    
-
-        result = minimize(to_minimize_with_grad, x, bounds=[(-np.pi*2,np.pi*2)]*len(x0), jac=True, method=method, tol=1e-6,
-                          options={'disp': None, 'maxls': 20, 'iprint': -1, 'eps': 1e-7,'ftol':1e-6, 'maxiter': 1500, 'maxcor': 12, 'maxfun': 1500}) 
+        result = minimize(to_minimize_with_grad, x, bounds=[(-np.pi*2, np.pi*2)]*len(x0), jac=True, method=method, tol=1e-6,
+                          options={'disp': None, 'maxls': 20, 'iprint': -1, 'eps': 1e-7, 'ftol': 1e-6, 'maxiter': 1500, 'maxcor': 12, 'maxfun': 1500})
         print("Optimization result:", result)
         print(f'Optimization converged: {result.success}')
 
@@ -141,42 +49,39 @@ class trainer_model(Sequential):
         print(f'stored f_list: {f_list}')
         print(f'stored gradient_list: {gradient_list[-1]}')
 
-        
         if result.success:
             print("Optimization converged successfully.")
         else:
-             print("Optimization did not converge. Reason:", result.message)
+            print("Optimization did not converge. Reason:", result.message)
 
-        return result.x 
-
-
+        return result.x
 
     @staticmethod
-    def default_model(input_shape:tuple):
-        initializer = keras.initializers.he_normal(seed=10)
-        return trainer_model(
+    def default_model(input_shape: tuple):
+        return TrainerModel(
             layers=[
-                klayers.Input(input_shape),
-                klayers.Dense(96, activation='elu',
-                                   kernel_initializer=initializer,
-                                   kernel_regularizer=keras.regularizers.l2(1e-8),
-                                   ),
-                klayers.Dense(64, activation='elu'),
-                klayers.Dense(18, activation='elu'),
-                klayers.Dense(10, activation='elu'),
-                klayers.Dense(1),
-                ],
-                name='default_model'
-            )
+                nn.Linear(input_shape[0], 96),
+                nn.ELU(),
+                nn.Linear(96, 64),
+                nn.ELU(),
+                nn.Linear(64, 18),
+                nn.ELU(),
+                nn.Linear(18, 10),
+                nn.ELU(),
+                nn.Linear(10, 1)
+            ],
+            name='default_model'
+        )
 
     @staticmethod
-    def simple_model(input_shape:tuple):
-        return trainer_model(
+    def simple_model(input_shape: tuple):
+        return TrainerModel(
             layers=[
-                klayers.Input(input_shape),
-                klayers.Dense(32, activation='elu'),
-                klayers.Dense(8, activation='sigmoid'),
-                klayers.Dense(1),
-                ],
-                name='simple_model'
-            )
+                nn.Linear(input_shape[0], 32),
+                nn.ELU(),
+                nn.Linear(32, 8),
+                nn.Sigmoid(),
+                nn.Linear(8, 1)
+            ],
+            name='simple_model'
+        )
--- optimizer.py
+++ optimizer.py
@@ -1,63 +1,55 @@
-from mimetypes import init
 from typing import List
 from scipy.optimize import minimize, OptimizeResult
-# from scipy.special import softmax   
-# from scipy.special import softmax   
-from nn_trainer import trainer_model
+from nn_trainer import TrainerModel
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau
-from keras.optimizers import Adam
-import tensorflow as tf
-# import tensorflow_addons as tfa
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 
 import sys
 import os
 import random
 
 # Set the seed value for reproducibility
-SEED = 42
+# SEED = 42
 
-# Set the PYTHONHASHSEED environment variable
-os.environ['PYTHONHASHSEED'] = str(SEED)
+# # Set the PYTHONHASHSEED environment variable
+# os.environ['PYTHONHASHSEED'] = str(SEED)
 
-# Set random seed for Python's `random` module
-random.seed(SEED)
+# # Set random seed for Python's `random` module
+# random.seed(SEED)
 
-# Set random seed for NumPy
-np.random.seed(SEED)
+# # Set random seed for NumPy
+# np.random.seed(SEED)
 
-# Set random seed for TensorFlow
-tf.random.set_seed(SEED)
+# # Set random seed for PyTorch
+# torch.manual_seed(SEED)
 
-# Additional configuration for GPU determinism in TensorFlow
-# Make sure operations involving cuDNN are deterministic
-os.environ['TF_DETERMINISTIC_OPS'] = '1'
-os.environ['TF_CUDNN_DETERMINISTIC'] = '1'
-
-
-
-
+# Additional configuration for GPU determinism in PyTorch
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = True
 
 class Optimizer:
 
-    _available_methods = ['Neural Network', 'Nelder-Mead', 'Powell', 'CG', 'BFGS','linear_regression','random search','Adam']
+    _available_methods = ['Neural Network', 'Nelder-Mead', 'Powell', 'CG', 'BFGS', 'linear_regression', 'random search', 'Adam']
 
     @staticmethod
     def list_methods():
         return Optimizer._available_methods
 
-    def __init__(self, method:str='Neural Network') -> None:
+    def __init__(self, method: str = 'Neural Network') -> None:
         self.method = method
-        
         self.saved_path = None
-    
+
     @property
     def get_path_x(self):
         if self.saved_path is None:
             return None
         else:
             return self.path_x
+
     @property
     def get_path_y(self):
         if self.saved_path is None:
@@ -66,12 +58,12 @@ class Optimizer:
             return self.path_y
 
     def optimize(self,
-             func,
-             x0,
-             callback=None,
-             record_path: bool = True,
-             method: str = None,
-             **kwargs) -> OptimizeResult:
+                 func,
+                 x0,
+                 callback=None,
+                 record_path: bool = True,
+                 method: str = None,
+                 **kwargs) -> OptimizeResult:
 
         if record_path:
             self.path_x = []
@@ -105,9 +97,8 @@ class Optimizer:
         else:
             raise ValueError(f'Unknown optimization method: {method}')
 
-
-    def _NN_opt(self,func, x0, callback=None, **kwargs):
-        # optimize using neural network
+    def _NN_opt(self, func, x0, callback=None, **kwargs):
+        # Optimize using neural network
         para_size = len(x0)
         res = OptimizeResult()
         res.nfev = 0
@@ -120,209 +111,112 @@ class Optimizer:
         batch_size = kwargs.get('batch_size', 16)
         verbose = kwargs.get('verbose', 0)
         nn_models = kwargs.get('NN_Models', [
-        trainer_model.default_model((para_size,)),
-        trainer_model.simple_model((para_size,)),
-    ])
-
-      
-
+            TrainerModel.default_model((para_size,)),
+            TrainerModel.simple_model((para_size,))
+        ])
 
         sample_y = np.array([])
-        sample_x = np.array([]).reshape(0,para_size)
-        # boundry for the parameters
-        # bound_low = -2 * np.pi
-        # bound_high = 2 * np.pi
-        optimal = [None,1] # [opt_para, opt_y]
+        sample_x = np.empty((0, para_size))
+        optimal = [None, float('inf')]  # [opt_para, opt_y]
 
-        for para in init_data: # generate the initial points
-
+        # Generate initial sample data
+        for para in init_data:
             y = func(para)
             if y < optimal[1]:
                 optimal = [para, y]
-        
-            # prid_data = np.vstack([para, para+ np.pi*2,para - np.pi*2])
-            # prid_data = np.clip(prid_data, bound_low, bound_high)
-            # len_x = prid_data.shape[0]
-            # sample_x = np.vstack([sample_x, prid_data])
-            # sample_y = np.append(sample_y, [y]*len_x)
-            sample_x = np.append(sample_x, [para], axis=0)
+            sample_x = np.vstack([sample_x, para])
             sample_y = np.append(sample_y, y)
-          
 
         print(f'Training with the neural networks')
-        # flush the output
         sys.stdout.flush()
-        
-                    
+
         for model in nn_models:
-            model.summary()
-        # flush the output
+            print(model)
             sys.stdout.flush()
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            # print("early stopping")
-            # early_stopping = EarlyStopping(
-            #     monitor='loss',
-            #     # min_delta=1e-6,
-            #     patience=100,  # Stop training if no improvement in 5 epochs
-            #     mode='min',
-            #     restore_best_weights=True
-            # )
-            # print("reduce_lr")
-            # reduce_lr = ReduceLROnPlateau(
-            #     monitor='loss', factor=0.5, patience=10, min_lr=1e-6, verbose=1
-            # )
-            # clr = tfa.optimizers.CyclicalLearningRate(
-            #     initial_learning_rate=1e-4,
-            #     maximal_learning_rate=1e-2,
-            #     step_size=2000,
-            #     scale_fn=lambda x: 1/(2.**(x-1)),
-            #     scale_mode="cycle",
-            # )
 
             for iteration in range(max_iter):
-
-              
                 res.nit += 1
-                
-                for model in nn_models:
-                    
-                    model.compile(optimizer=Adam(learning_rate=1e-4), 
-                                    loss='mse', metrics=['mae'])
-                    
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                    print(f"epochs: {classical_epochs}")
-                    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                    print(f"batch size: {batch_size}")
-                    
-                    fit_his = model.fit(sample_x,
-                                sample_y,
-                                epochs=classical_epochs,
-                                batch_size=batch_size,
-                                verbose=verbose,
-                                # callbacks=[early_stopping]
-                                )
-                    # print the training history
-                    # Print loss and epoch informatio
-                    print("====================================================")
-                    print(f"Iteration {iteration + 1}/{max_iter}")
-                    print(f"batch")
-                    print(f'epochs: {classical_epochs}')
-                    print(f'sample size: {len(sample_x)}')
-                    for epoch, loss in enumerate(fit_his.history['loss'], start=1):
-                        print(f"Epoch {epoch}/{classical_epochs} - Loss: {loss:.1e}")
-                    sys.stdout.flush()
-                    print("====================================================")
-                   
+                print(f'Iteration {iteration + 1}/{max_iter}')
 
+                criterion = nn.MSELoss()
+                optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
+                model.train()
+                for epoch in range(classical_epochs):
+                    total_loss = 0.0
+                    permutation = torch.randperm(sample_x.shape[0])
 
-                    x0 = optimal[0] 
-                    # + np.random.normal(0, 0.02, para_size)
-                    
-                    sys.stdout.flush()
-                    
-                    prediction0 = model.back_minimize(x0=x0,method='L-BFGS-B', verbose=verbose)
-                    # print(f'ite:::::::::::::::::::::::::::::::::::::::: {res.nit}') 
+                    for i in range(0, sample_x.shape[0], batch_size):
+                        indices = permutation[i:i + batch_size]
+                        batch_x, batch_y = sample_x[indices], sample_y[indices]
 
-                    # if np.linalg.norm(prediction0 - x0) > 1e-3:
-                    #     print(f'Prediction is different from x0: {np.linalg.norm(prediction0 - x0)}')
-                    
-                    # else:
-                    #     print(f'Prediction is too close to x0: {np.linalg.norm(prediction0 - x0)}')
-                    #     x0 = np.random.uniform(-np.pi/2,np.pi/2, len(x0)) 
-                    #     prediction0 = x0
-                    # Evaluate on real quantum computer
-                    y0 = func(prediction0)
-                    res.nfev += 1
-                    # if np.abs(y0)> 1e-4:
-                    #     yr = func(x0)
-                    #     if yr < y0:
-                    #         print(f'Random point is better than prediction: {yr} < {y0}')
-                    #         prediction0 = x0
-                    #         y0 = yr
-                    # print(f'data size ({model.name}):', len(sample_x))
-                    # sys.stdout.flush()
-                    if y0 < optimal[1]:
-                        optimal = [prediction0, y0]
-                    # sample_x = np.append(sample_x, [prediction0], axis=0)
-                    # sample_y = np.append(sample_y, y0)
-        
-                    # Gather all prediction variations (original, +4π, and -4π)
-                    predictions = np.vstack([prediction0, prediction0+ np.pi*2,prediction0 - np.pi*2])
-                    # predictions = np.clip(predictions, bound_low, bound_high)
-                    len_p = predictions.shape[0]
-                    
-                    
-                                    
-                    sample_x = np.concatenate([sample_x, predictions], axis=0)
-                    # Extend sample_y with the corresponding y0 values (same for each variation)
-                    sample_y = np.concatenate([sample_y, [y0] * len_p])
+                        inputs = torch.tensor(batch_x, dtype=torch.float64)
+                        targets = torch.tensor(batch_y, dtype=torch.float64).unsqueeze(-1)
 
-                    
-                    # random points for the next point
-                    # if np.abs(y0-optimal[1])< 1e-5 and optimal[1]>1e-3:
-                    #     for i in range(2):
-                    #         print(f'Cost is too close to optimal: {np.abs(y0-optimal[1])}')
-                    #         x0 = np.random.uniform(-np.pi*2,np.pi*2, len(x0)) 
-                    #         y0 = func(x0)
-                    #         if y0 < optimal[1]:
-                    #             optimal = [x0, y0]
-                    #         sample_x = np.append(sample_x, [x0], axis=0)
-                    #         sample_y = np.append(sample_y, y0)
-       
+                        optimizer.zero_grad()
+                        outputs = model(inputs)
+
+                        loss = criterion(outputs, targets)
+                        total_loss += loss.item()
+
+                        loss.backward()
+                        optimizer.step()
+
+                    # Print average loss for the epoch if verbose
+                    if verbose:
+                        avg_loss = total_loss / (sample_x.shape[0] // batch_size)
+                        
+                        print(f'Epoch {epoch + 1}/{classical_epochs}, Average Loss: {avg_loss:1e}')
+                        sys.stdout.flush()
+
+                # Prediction and updating optimal parameters
+                x0 = optimal[0]
+                prediction0 = model.back_minimize(x0=x0, method='L-BFGS-B', verbose=verbose)
+                y0 = func(prediction0)
+                res.nfev += 1
+
+                if y0 < optimal[1]:
+                    optimal = [prediction0, y0]
+
+                predictions = np.vstack([prediction0, prediction0 + np.pi * 2, prediction0 - np.pi * 2])
+                sample_x = np.concatenate([sample_x, predictions], axis=0)
+                sample_y = np.concatenate([sample_y, [y0] * predictions.shape[0]])
+
         res.x = np.copy(optimal[0])
         res.fun = np.copy(optimal[1])
-    
-        return res
-    
 
-    
-    def _random_search(self,func, x0, callback=None, **kwargs):
-        # optimize using neural network
+        return res
+
+
+    def _random_search(self, func, x0, callback=None, **kwargs):
         para_size = len(x0)
         res = OptimizeResult()
         res.nfev = 0
         res.nit = 0
 
-        # Define the default values
-        if 'init_data' in kwargs:
-            init_data:List = kwargs['init_data']
-        else:
-            init_data = np.random.uniform(-10,10,(60,para_size))
-
-        if 'max_iter' in kwargs:
-            max_iter:int = kwargs['max_iter']
-        else:
-            max_iter:int = 20
-
+        init_data = kwargs.get('init_data', np.random.uniform(-10, 10, (60, para_size)))
+        max_iter = kwargs.get('max_iter', 20)
 
         sample_x = init_data
         sample_y = np.array([])
 
-        optimal = [None,1] # [opt_para, opt_y]
+        optimal = [None, 1]  # [opt_para, opt_y]
 
-        for para in sample_x: # generate the initial points
-
+        for para in sample_x:  # generate the initial points
             y = func(para)
             if y < optimal[1]:
                 optimal = [para, y]
             sample_y = np.append(sample_y, y)
 
-        print(f'Training with  random search')
-        # flush the output
+        print(f'Training with random search')
         sys.stdout.flush()
-
-        
 
         for _ in range(max_iter):
             res.nit += 1
 
             x0 = optimal[0] + np.random.normal(0, .02, para_size)
-
-            
             prediction = x0
 
-            # Evaluate on real quantum computer
             y = func(prediction)
             res.nfev += 1
             sys.stdout.flush()
@@ -331,19 +225,8 @@ class Optimizer:
                 optimal = [prediction, y]
             sample_x = np.append(sample_x, [prediction], axis=0)
             sample_y = np.append(sample_y, y)
-            if y < optimal[1]:
-                optimal = [prediction, y]
-            sample_x = np.append(sample_x, [prediction], axis=0)
-            sample_y = np.append(sample_y, y)
-        
+
         res.x = np.copy(optimal[0])
         res.fun = np.copy(optimal[1])
-        # res.message = message
-        # res.nit = i + 1
+
         return res
-    
-
-
-    
-
-
