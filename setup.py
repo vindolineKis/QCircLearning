314e51364ae1372132f737012e7582ed65860d12
Canoming <canoming@163.com>
2024-11-18 04:21:21 +0800

    clean up documents and correct dependencies

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='QCircLearning',
       packages=find_packages(exclude=["*.tests","*.tests.*","tests.*","tests"]),
       install_requires=[
           'qiskit >= 0.45.0, < 1.0.0',
-          'keras >= 3.0.0',
+          'torch >= 2.0.0',
           'pylatexenc >= 2.8',
           'matplotlib >= 3.0.0',
       ],
